26e31fa58aec8891443e2ac94aad1634053c023a
c4nkn <cankahraman05@gmail.com>
2024-05-30 20:37:29 +0300

    Fixed file system completely, i hope

--- src-tauri/src/file.rs
+++ src-tauri/src/file.rs
@@ -1,15 +1,18 @@
 use std::fs;
+use std::path::Path;
 
 // Read file content in given path
 #[tauri::command]
 pub fn read_file_content(path: String) -> Result<String, String> {
+    let path = Path::new(&path);
+
     if !path.exists() {
         return Err(format!("File does not exist: {:?}", path));
     }
     if !path.is_file() {
         return Err(format!("Path is not a file: {:?}", path));
     }
-    
+
     match fs::read_to_string(&path) {
         Ok(content) => Ok(content),
         Err(_) => Err("Failed to read file content.".to_string()),
@@ -18,9 +21,8 @@ pub fn read_file_content(path: String) -> Result<String, String> {
 
 // Get files and folders in given directory
 #[tauri::command]
-pub fn get_files_and_subdirs(path: &str) -> Vec<String> {
-    let paths = fs::read_dir(path).unwrap();
-    let mut entries = Vec::new();
+pub fn get_files_and_subdirs(path: &str) -> Result<Vec<String>, String> {
+    let path = Path::new(path);
 
     if !path.exists() {
         return Err(format!("Directory does not exist: {:?}", path));
@@ -28,6 +30,9 @@ pub fn get_files_and_subdirs(path: &str) -> Vec<String> {
     if !path.is_dir() {
         return Err(format!("Path is not a directory: {:?}", path));
     }
+
+    let paths = fs::read_dir(path).unwrap();
+    let mut entries = Vec::new();
 
     for entry in paths {
         let entry = entry.unwrap();
@@ -41,5 +46,5 @@ pub fn get_files_and_subdirs(path: &str) -> Vec<String> {
         }
     }
 
-    entries
+    Ok(entries)
 }

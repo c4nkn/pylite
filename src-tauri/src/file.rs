649ef560e76ae63acc3bd64f769e2f61fd2cb8ad
c4nkn <cankahraman05@gmail.com>
2024-05-30 20:35:22 +0300

    Fixed open directory func

--- src-tauri/src/file.rs
+++ src-tauri/src/file.rs
@@ -3,6 +3,13 @@ use std::fs;
 // Read file content in given path
 #[tauri::command]
 pub fn read_file_content(path: String) -> Result<String, String> {
+    if !path.exists() {
+        return Err(format!("File does not exist: {:?}", path));
+    }
+    if !path.is_file() {
+        return Err(format!("Path is not a file: {:?}", path));
+    }
+    
     match fs::read_to_string(&path) {
         Ok(content) => Ok(content),
         Err(_) => Err("Failed to read file content.".to_string()),
@@ -14,6 +21,13 @@ pub fn read_file_content(path: String) -> Result<String, String> {
 pub fn get_files_and_subdirs(path: &str) -> Vec<String> {
     let paths = fs::read_dir(path).unwrap();
     let mut entries = Vec::new();
+
+    if !path.exists() {
+        return Err(format!("Directory does not exist: {:?}", path));
+    }
+    if !path.is_dir() {
+        return Err(format!("Path is not a directory: {:?}", path));
+    }
 
     for entry in paths {
         let entry = entry.unwrap();
